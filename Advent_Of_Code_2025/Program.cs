7641188b578d601fe830ada2f5bac8da5f070dfb
L. Taburnal <179358946+OpaliteArchitect@users.noreply.github.com>
2025-12-07 03:39:57 -0800

    Day 1 solutions

---
+++ Advent_Of_Code_2025/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Day 1
+using Advent_Of_Code_2025.Day1;
+
+string path_1 = "../../../Day1Puzzles/Puzzle1Input";
+var inputs = await Day1Puzzles.Read(path_1);
+
+var answer1 = Day1Puzzles.SolvePuzzle1(inputs);
+Console.WriteLine($"Puzzle 1: {answer1}");
+
+var answer2 = Day1Puzzles.SolvePuzzle2(inputs);
+Console.WriteLine($"Puzzle 2: {answer2}");
+*/
