584582550df5a548a4bf045600b437c0b731fffe
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2025-12-08 01:52:40 -0800

    fixed day 2 variable names

--- Advent_Of_Code_2025/Program.cs
+++ Advent_Of_Code_2025/Program.cs
@@ -11,15 +11,13 @@ var answer1_2 = Day1Puzzles.SolvePuzzle2(inputs);
 Console.WriteLine($"Day 1 Puzzle 2: {answer1_2}");
 */
 
-/* Day 2
 using Advent_Of_Code_2025.Day2;
 
 string path_2 = "../../../Day2/Day2Input";
 var inputs = await Day2Puzzles.Read(path_2);
 
-var answer1_1 = Day2Puzzles.SolvePuzzle1(inputs);
-Console.WriteLine($"Day 2 Puzzle 1: {answer1_1}");
+var answer2_1 = Day2Puzzles.SolvePuzzle1(inputs);
+Console.WriteLine($"Day 2 Puzzle 1: {answer2_1}");
 
-var answer1_2 = Day2Puzzles.SolvePuzzle2(inputs);
-Console.WriteLine($"Day 2 Puzzle 2: {answer1_2}");
-*/
+var answer2_2 = Day2Puzzles.SolvePuzzle2(inputs);
+Console.WriteLine($"Day 2 Puzzle 2: {answer2_2}");
