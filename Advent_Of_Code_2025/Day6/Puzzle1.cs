29e4e4d02de91dbefa5e23c67de168c47ea98331
L. Taburnal <179358946+OpaliteArchitect@users.noreply.github.com>
2025-12-09 08:46:00 -0800

    Day 6 solutions

---
+++ Advent_Of_Code_2025/Day6/Puzzle1.cs
@@ -0,0 +1,52 @@
+﻿namespace Advent_Of_Code_2025.Day6
+{
+    internal partial class Day6Puzzles
+    {
+        private const int OPERAND_LINES = 4;
+        public static async Task<string[]> Read(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Ensure file path is correct");
+            }
+            return await File.ReadAllLinesAsync(path);
+        }
+
+        public static long SolvePuzzle1(string[] inputs)
+        {
+            List<int[]> operands = inputs.Take(OPERAND_LINES)
+                .Select(line =>
+                {
+                    int[] num = line
+                        .Split(' ')
+                        .Where(num => !string.IsNullOrWhiteSpace(num))
+                        .Select(int.Parse)
+                        .ToArray();
+                    return num;
+                })
+                .ToList();
+
+            char[] operators = inputs[OPERAND_LINES]
+                .Split(' ')
+                .Where(sign => !string.IsNullOrWhiteSpace(sign))
+                .Select(char.Parse)
+                .ToArray();
+
+            long answer = 0;
+
+            for (int i = 0; i < operands[0].Length; i++)
+            {
+                long columnAnswer = operators[i] switch
+                {
+                    '+' => operands.Select(num => (long)num[i]).Sum(),
+                    '*' => operands.Select(num => (long)num[i]).Aggregate((product, multiplier) => product * multiplier),
+                    _ => throw new InvalidOperationException("Plus and times only")
+                };
+
+                answer += columnAnswer;
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Advent_Of_Code_2025/Day6/Puzzle2.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace Advent_Of_Code_2025.Day6
+{
+    internal partial class Day6Puzzles
+    {
+        public static long SolvePuzzle2(string[] inputs)
+        {
+            string[] operandsMap = inputs.Take(OPERAND_LINES).ToArray();
+
+            char[] operators = inputs[OPERAND_LINES]
+                .Split(' ')
+                .Where(op => !string.IsNullOrWhiteSpace(op))
+                .Select(char.Parse)
+                .ToArray();
+
+            long answer = 0;
+
+            int group = 0;
+            List<long> operands = [];
+            StringBuilder operandRaw;
+            long groupAnswer;
+            for (int i = 0; i < operandsMap[0].Length; i++)
+            {
+                operandRaw = new();
+                for (int j = 0; j < OPERAND_LINES; j++)
+                {
+                    char digit = operandsMap[j][i];
+                    if (char.IsNumber(digit))
+                    {
+                        operandRaw.Append(digit);
+                    }
+                }
+
+                if (operandRaw.Length is not 0)
+                {
+                    operands.Add(long.Parse(operandRaw.ToString()));
+                }
+                else
+                {
+                    groupAnswer = operators[group] switch
+                    {
+                        '+' => operands.Aggregate((total, addend) => total + addend),
+                        '*' => operands.Aggregate((product, multiplier) => product * multiplier),
+                        _ => throw new InvalidOperationException("Plus and times only")
+                    };
+
+                    answer += groupAnswer;
+
+                    group++;
+                    operands.Clear();
+                }
+            }
+
+            groupAnswer = operators[group] switch
+            {
+                '+' => operands.Aggregate((total, addend) => total + addend),
+                '*' => operands.Aggregate((product, multiplier) => product * multiplier),
+                _ => throw new InvalidOperationException("Plus and times only")
+            };
+
+            answer += groupAnswer;
+
+            return answer;
+        }
+    }
+}
