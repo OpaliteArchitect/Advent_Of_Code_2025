6b8c383b04df5d31ee5f248bc58d73fbd8cfce52
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2025-12-09 06:17:24 -0800

    Day 5 solutions

---
+++ Advent_Of_Code_2025/Day5/Ranges.cs
@@ -0,0 +1,8 @@
+﻿namespace Advent_Of_Code_2025.Day5
+{
+    internal record Ranges(long _start, long _end)
+    {
+        public long Start { get; set; } = _start;
+        public long End { get; set; } = _end;
+    }
+}
