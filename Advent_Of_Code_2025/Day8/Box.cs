984ed4ec13ef685c16ba659c53dfc11efb77a49e
L. Taburnal <179358946+OpaliteArchitect@users.noreply.github.com>
2025-12-12 04:26:10 -0800

    Day 8 solutions

---
+++ Advent_Of_Code_2025/Day8/Box.cs
@@ -0,0 +1,4 @@
+﻿namespace Advent_Of_Code_2025.Day8
+{
+    internal record struct Box(int X, int Y, int Z);
+}
---
+++ Advent_Of_Code_2025/Day8/Pair.cs
@@ -0,0 +1,4 @@
+﻿namespace Advent_Of_Code_2025.Day8
+{
+    internal record struct Pair(Box A, Box B, long DistanceSquared);
+}
